1acf2b865c8de8c40970d2f96423012cb1af7aa2
Patodia <ppatodia@paypal.com>
2020-05-14 12:52:30 +0530

    first springboot for woc backend

---
+++ src/main/java/com/woc/datahandler/DataHandlerApplication.java
@@ -0,0 +1,15 @@
+package com.woc.datahandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.woc.controller.MainController;
+
+@SpringBootApplication
+public class DataHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainController.class, args);
+	}
+
+}
